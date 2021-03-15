29d74f4cdd21a2b21ff5c119b33eb9d7b513cc13
Arun kumar G <g.aruniyer@gmail.com>
2021-03-15 11:55:57 +0530

    Add files via upload

---
+++ SetRepoProperty.groovy
@@ -0,0 +1,49 @@
+import org.artifactory.repo.RepoPath
+import org.artifactory.search.aql.AqlResult
+import org.artifactory.repo.RepoPathFactory
+
+
+
+// Example REST API calls:
+// curl -X POST -v -u admin:password "http://localhost:8082/artifactory/api/plugins/execute/setPropertyValuesForRepository?
+// params=propertyName=artifact.developer;propertyValue=arunkg;repo=docker-local-akg"
+
+executions {
+    setPropertyValues() { params ->
+        propertyName = params?.get('propertyName')?.get(0) as String
+        propertyValue = params?.get('propertyValue')?.get(0) as String
+        repo = params?.get('repo')?.get(0) as String
+        dryRun = new Boolean(params?.get('dryRun')?.get(0))
+        setPropertyValues(propertyName, propertyValue, repo, dryRun)
+    }
+}
+
+private def setPropertyValues(propertyName, propertyValue, repo, dryRun) {
+    log.info "Searching for files that are missing the property $propertyName in $repo"
+
+    def count = 0
+    def aql = "items.find({\"repo\":{ \"\$eq\":\""+ repo +"\" }, \"property.key\":{ \"\$ne\":\"" + propertyName + "\"}})"
+
+    searches.aql(aql) { AqlResult result ->
+        result.each { Map item ->
+            String itemPath = item.path + "/" + item.name
+            log.info "Found: $itemPath"
+            RepoPath repoPath = RepoPathFactory.create(repo, itemPath)
+
+            log.info "Found: $itemPath"
+            log.info "adding Property: $propertyName = $propertyValue"
+            if (!dryRun) {
+                repositories.setProperty(repoPath, propertyName, propertyValue)
+            }
+            count++
+        }
+    }
+
+    if (count > 0) {
+        log.info ("Succesfully tagged $count files (dryRun: $dryRun)")
+    } else {
+        log.info("All files is tagged with the property: '$propertyName' already. Did not do anything")
+    }
+
+    status = 200
+}
